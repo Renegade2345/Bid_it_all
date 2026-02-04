fe614a21a3642de82a5ba4bb29132618e9a4ff32
Renegade <singharpit.asb@gmail.com>
2026-02-04 08:40:59 +0530

    Redis changes for prod

--- backend/src/redis.js
+++ backend/src/redis.js
@@ -1,18 +1,15 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: "redis://127.0.0.1:6379"
+  url: process.env.REDIS_URL
 });
 
 redisClient.on("error", (err) => {
   console.error("❌ Redis error:", err);
-}); 
+});
 
-export async function connectRedis() {
-  if (!redisClient.isOpen) {
-    await redisClient.connect();
-    console.log("✅ Connected to Redis");
-  }
-}
+await redisClient.connect();
+
+console.log("✅ Connected to Redis");
 
 export default redisClient;
