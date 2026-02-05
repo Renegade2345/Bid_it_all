31447f6cdc25cd6b5536af8f532b674e7b6cf467
Renegade <singharpit.asb@gmail.com>
2026-02-05 13:41:59 +0530

    some other required changes in socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://bidding-wars-evvz.onrender.com");
+const socket = io("https://bidding-wars-evvz.onrender.com",{
+    transports:["websocket"]
+});
 
 
 export default socket;
