d1dbf6addec7d6b5a61dc595e8eda898c2995ee2
Renegade <singharpit.asb@gmail.com>
2026-02-05 08:44:19 +0530

    modified app.js for redis issues

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,35 +3,68 @@ import cors from "cors";
 import { v4 as uuidv4 } from "uuid";
 import redisClient, { connectRedis } from "./redis.js";
 
-
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 await connectRedis();
 
 
 
-const items = [
-  {
-    id: uuidv4(),
-    title: "MacBook Pro M3",
-    startingPrice: 1200,
-    currentBid: 1200,
-    auctionEndTime: Date.now() + 60 * 60 * 1000 
-  },
-  {
-    id: uuidv4(),
-    title: "iPhone 15 Pro",
-    startingPrice: 900,
-    currentBid: 900,
-    auctionEndTime: Date.now() + 60 * 60 * 1000
+async function seedAuctions() {
+  const existing = await redisClient.keys("item:*");
+
+  if (existing.length > 0) {
+    console.log("Auctions already seeded");
+    return;
   }
-];
+
+  const items = [
+    {
+      id: uuidv4(),
+      title: "MacBook Pro M3",
+      startingPrice: 1200,
+      currentBid: 1200,
+      auctionEndTime: Date.now() + 60 * 60 * 1000,
+      highestBidder: ""
+    },
+    {
+      id: uuidv4(),
+      title: "iPhone 15 Pro",
+      startingPrice: 900,
+      currentBid: 900,
+      auctionEndTime: Date.now() + 60 * 60 * 1000,
+      highestBidder: ""
+    }
+  ];
+
+  for (const item of items) {
+    await redisClient.hSet(`item:${item.id}`, item);
+  }
+
+  console.log("Auctions seeded in Redis");
+}
+
+await seedAuctions();
 
 
-app.get("/items", (req, res) => {
+
+app.get("/items", async (req, res) => {
+  const keys = await redisClient.keys("item:*");
+  const items = [];
+
+  for (const key of keys) {
+    const item = await redisClient.hGetAll(key);
+
+    items.push({
+      ...item,
+      startingPrice: Number(item.startingPrice),
+      currentBid: Number(item.currentBid),
+      auctionEndTime: Number(item.auctionEndTime)
+    });
+  }
+
   res.json(items);
 });
 
